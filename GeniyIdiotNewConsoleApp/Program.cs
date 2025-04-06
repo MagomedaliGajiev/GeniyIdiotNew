e8d117482b3a5727235430d8bf0867e3450f77f5
Magomedali Gajiev <mag198421@gmail.com>
2025-04-06 15:12:16 +0300

    Вопросы в приложении задаются в произвольном порядке и не повторяются.

--- GeniyIdiotNewConsoleApp/Program.cs
+++ GeniyIdiotNewConsoleApp/Program.cs
@@ -5,64 +5,70 @@
         static void Main(string[] args)
         {
             var countQuestions = 5;
-            var questions = GetQuestions(countQuestions);
-            var answers = GetAnswers(countQuestions);
+            var questions = GetQuestions();
+            var answers = GetAnswers();
 
             var rightAnswerscount = 0;
+            
 
-            Random random = new Random();
+            // Создаем и перемешиваем индексы вопросов
+            var questionIndices = Enumerable.Range(0, countQuestions).ToList();
+            Shuffle(questionIndices);
 
             for (int i = 0; i < countQuestions; i++)
             {
                 Console.WriteLine("Вопрос №" + (i + 1));
 
-                var randomQuestionIndex = random.Next(0, countQuestions);
-                Console.WriteLine(questions[randomQuestionIndex]);
+                var currentIndex = questionIndices[i];
+                Console.WriteLine(questions[currentIndex]);
 
                 var userAnswer = Convert.ToInt32(Console.ReadLine());
-
-                var rightAnswer = answers[randomQuestionIndex];
+                var rightAnswer = answers[currentIndex];
 
                 if (userAnswer == rightAnswer)
                 {
                     rightAnswerscount++;
                 }
             }
+
             var diagnosis = GetDiagnosis(rightAnswerscount);
 
             Console.WriteLine($"Количество правильных ответов: {rightAnswerscount}");
             Console.WriteLine($"Ваш диагноз:{diagnosis}");
         }
-        private static string[] GetQuestions(int countQuestions)
+
+        private static void Shuffle<T>(IList<T> list)
         {
-            var questions = new string[countQuestions];
-            questions[0] = "Сколько будет два плюс два умноженное на два?";
-            questions[1] = "Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?";
-            questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
-            questions[3] = "Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?";
-            questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
-            return questions;
+            var random = new Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                var k = random.Next(n + 1);
+                (list[n], list[k]) = (list[k], list[n]);
+            }
         }
-        private static int[] GetAnswers(int countQuestions)
+
+        private static string[] GetQuestions()
         {
-            var answers = new int[countQuestions];
-            answers[0] = 6;
-            answers[1] = 9;
-            answers[2] = 25;
-            answers[3] = 60;
-            answers[4] = 2;
-            return answers;
+            return new string[]
+            {
+                "Сколько будет два плюс два умноженное на два?",
+                "Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?",
+                "На двух руках 10 пальцев. Сколько пальцев на 5 руках?",
+                "Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?",
+                "Пять свечей горело, две потухли. Сколько свечей осталось?"
+            };
         }
+
+        private static int[] GetAnswers()
+        {
+            return new int[] { 6, 9, 25, 60, 2 };
+        }
+
         private static string GetDiagnosis(int rightAnswersCount)
         {
-            var diagnoses = new string[6];
-            diagnoses[0] = "кретин";
-            diagnoses[1] = "идиот";
-            diagnoses[2] = "дурак";
-            diagnoses[3] = "нормальный";
-            diagnoses[4] = "талант";
-            diagnoses[5] = "гений";
-
+            var diagnoses = new string[] { "кретин", "идиот", "дурак", "нормальный", "талант", "гений" };
             return diagnoses[rightAnswersCount];
         }
     }
