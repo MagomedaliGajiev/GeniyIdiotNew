0f90fec8a6b6c939859c8819cad1270a26ecd6dd
Magomedali Gajiev <mag198421@gmail.com>
2025-04-06 14:44:11 +0300

    Получение диагноза и получение ответов выделено в отдельные функции

--- GeniyIdiotNewConsoleApp/Program.cs
+++ GeniyIdiotNewConsoleApp/Program.cs
@@ -2,30 +2,13 @@
 {
     public class Program
     {
-        static string[] GetQuestions(int countQuestions)
-        {
-            string[] questions = new string[countQuestions];
-            questions[0] = "Сколько будет два плюс два умноженное на два?";
-            questions[1] = "Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?";
-            questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
-            questions[3] = "Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?";
-            questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
-            return questions;
-        }
-
-
         static void Main(string[] args)
         {
-            int countQuestions = 5;
-            string[] questions = GetQuestions(countQuestions);
-            int[] answers = new int[countQuestions];
-            answers[0] = 6;
-            answers[1] = 9;
-            answers[2] = 25;
-            answers[3] = 60;
-            answers[4] = 2;
+            var countQuestions = 5;
+            var questions = GetQuestions(countQuestions);
+            var answers = GetAnswers(countQuestions);
 
-            int countRightAnswers = 0;
+            var rightAnswerscount = 0;
 
             Random random = new Random();
 
@@ -33,29 +16,54 @@
             {
                 Console.WriteLine("Вопрос №" + (i + 1));
 
-                int randomQuestionIndex = random.Next(0, countQuestions);
+                var randomQuestionIndex = random.Next(0, countQuestions);
                 Console.WriteLine(questions[randomQuestionIndex]);
 
-                int userAnswer = Convert.ToInt32(Console.ReadLine());
+                var userAnswer = Convert.ToInt32(Console.ReadLine());
 
-                int rightAnswer = answers[randomQuestionIndex];
+                var rightAnswer = answers[randomQuestionIndex];
 
                 if (userAnswer == rightAnswer)
                 {
-                    countRightAnswers++;
+                    rightAnswerscount++;
                 }
             }
+            var diagnosis = GetDiagnosis(rightAnswerscount);
 
-            Console.WriteLine("Количество правильных ответов: " + countRightAnswers);
-
-            string[] diagnoses = new string[6];
+            Console.WriteLine($"Количество правильных ответов: {rightAnswerscount}");
+            Console.WriteLine($"Ваш диагноз:{diagnosis}");
+        }
+        private static string[] GetQuestions(int countQuestions)
+        {
+            var questions = new string[countQuestions];
+            questions[0] = "Сколько будет два плюс два умноженное на два?";
+            questions[1] = "Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?";
+            questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
+            questions[3] = "Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?";
+            questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
+            return questions;
+        }
+        private static int[] GetAnswers(int countQuestions)
+        {
+            var answers = new int[countQuestions];
+            answers[0] = 6;
+            answers[1] = 9;
+            answers[2] = 25;
+            answers[3] = 60;
+            answers[4] = 2;
+            return answers;
+        }
+        private static string GetDiagnosis(int rightAnswersCount)
+        {
+            var diagnoses = new string[6];
             diagnoses[0] = "кретин";
             diagnoses[1] = "идиот";
             diagnoses[2] = "дурак";
             diagnoses[3] = "нормальный";
             diagnoses[4] = "талант";
             diagnoses[5] = "гений";
-            Console.WriteLine("Ваш диагноз:" + diagnoses[countRightAnswers]);
+
+            return diagnoses[rightAnswersCount];
         }
     }
 }
