c5175f60db9ad3c566edc802fb86488ed3b25443
Magomedali Gajiev <mag198421@gmail.com>
2025-04-07 20:55:12 +0300

    Изменения

--- GeniyIdiotNewConsoleApp/Program.cs
+++ GeniyIdiotNewConsoleApp/Program.cs
@@ -1,32 +1,5 @@
-﻿using Newtonsoft.Json;
-
-namespace GeniyIdiotNewConsoleApp
+﻿namespace GeniyIdiotNewConsoleApp
 {
-    public static class FileProvider
-    {
-        public static void SaveToFile<T>(string path, T data)
-        {
-            var json = JsonConvert.SerializeObject(data, Formatting.Indented);
-            File.WriteAllText(path, json);
-        }
-
-        public static T LoadFromFile<T>(string path) where T : new()
-        {
-            if (File.Exists(path))
-            {
-                var json = File.ReadAllText(path);
-                return JsonConvert.DeserializeObject<T>(json) ?? new T();
-            }
-            return new T();
-        }
-
-        public static void AppendToFile<T>(string path, T item)
-        {
-            var list = LoadFromFile<List<T>>(path);
-            list.Add(item);
-            SaveToFile(path, list);
-        }
-    }
     class Program
     {
         static void Main(string[] args)
