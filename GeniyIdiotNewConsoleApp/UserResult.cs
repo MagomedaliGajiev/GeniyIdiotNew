c22ef60a772959e35b0a8cf8ac31991ae408cbd3
Магомедали Гаджиев <mag198421@gmail.com>
2025-04-08 13:53:38 +0300

    Изменил классс UserResult

--- GeniyIdiotNewConsoleApp/UserResult.cs
+++ GeniyIdiotNewConsoleApp/UserResult.cs
@@ -2,16 +2,16 @@
 {
     public class UserResult
     {
-        public string UserName { get; set; }
+        public User User { get; set; }
         public int CorrectAnswersCount { get; set; }
         public string Diagnosis { get; set; }
         public DateTime TestDateTime { get; set; }
 
         public UserResult() { }
 
-        public UserResult(string userName, int correctAnswersCount, string diagnosis, DateTime testDateTime)
+        public UserResult(User user, int correctAnswersCount, string diagnosis, DateTime testDateTime)
         {
-            UserName = userName;
+            User = user;
             CorrectAnswersCount = correctAnswersCount;
             Diagnosis = diagnosis;
             TestDateTime = testDateTime;
